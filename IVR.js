c4ff0ab31cba6d19e6afab665f43c4e0742a8366
bruno figueiredo <bruno.figueiredo@everis.com>
2016-10-07 17:08:55 +0100

    code restructured

--- IVR.js
+++ IVR.js
@@ -34,7 +34,95 @@ function requestJSONviaGET(requestedURL) {
     }
 }
 
+/**
+ * Get the caller details from the remote data source
+ * 
+ * @param {string} callerId The caller id that initiated this IVR
+ */
+function getCallerDetails(callerId){
+    var data = requestJSONviaGET("https://raw.githubusercontent.com/brunoshine/tropoSample/master/sample_data.json");
+    var filteredData = data.filter(function(el){
+        el.phone == callerId;
+    });
 
+    // // if its a know caller id get his name, and conversation language
+    var callerDetails = null;
+    if(filteredData && filteredData.length === 1){
+        callerDetails = filteredData[0];
+    }
+    return callerDetails;
+}
+/**
+ * Greets the user based on if his a known user or not.
+ * 
+ * @param {Object} knownCaller The known caller details, if any. 
+ */
+function greetUser(knownCaller){
+    var welcomeMessage = "Welcome to the Matrix.";
+    if(knownCaller != null){
+        // translate to caller native language
+        welcomeMessage = "Hello Mr. " + knownCaller.name + ". " + welcomeMessage;
+    }
+    say(welcomeMessage);
+    log("Said welcome message to " + currentCall.callerID);
+}
+
+function requestForAction(knownCaller){
+    var actionPickMessage = "With whom would you like to speak?";
+    var actionPickOptions = "";
+    if(knownCaller != null && knownCaller.contacts !== null){
+        for(var i=0; i < knownCaller.contacts.length; i++){
+            var contact = knownCaller.contacts[i];
+            var opt = i + 1;
+            // +=  IS NOT SUPPORTED
+            actionPickMessage = actionPickMessage + "Press " + opt + " for " + contact.name + ".";
+            actionPickOptions = actionPickOptions + opt + ","; 
+        }
+    }
+
+    actionPickMessage = actionPickMessage + " To speak to the operator please choose 0.";
+    actionPickOptions = actionPickOptions + "0";
+
+    log("created options list");
+    log(actionPickMessage);
+    log(actionPickOptions);
+
+    log("asking...");
+    var result=ask(actionPickMessage, {
+    choices:actionPickOptions
+    });
+    log("picked operation:" + result.value);
+
+    return parseInt(result.value);
+}
+
+/**
+ * Redirects the caller to the selected operator
+ * 
+ * @param {Object} knownCaller The caller details, if found on the remote data source.
+ * @param {number} operatorId The option identifier selected by the caller.
+ */
+function redirectCall(knownCaller, operatorId){
+    var selectedOperator = "+351999000888"; // by default go to Operator.    
+
+    if(operatorId !== 0){
+        selectedOperator = knownCaller.contacts[operatorId-1].phone;
+        say("You picked to speak to " + knownCaller.contacts[operatorId-1].name + ".");
+        log("said transfing to picked contact");
+    }else{
+        say("You picked to speak to the Operator.");
+        log("said transferring to operator");
+    }
+
+    // redirect call to selected contact (AM or Operator)
+
+    //translate transfer message to caller native language
+    var transferMessage ="Transferring you now, please wait."; 
+
+    // transfer
+    say(transferMessage);
+    //transfer(selectedOperator);
+}
 
 // get caller id
 var callerId = currentCall.callerID;
@@ -42,82 +130,12 @@ log("Call was from: " + currentCall.callerID);
 
 // query data storage to see if its a known caller id
 // TODO: Move to Azure Tables
-var data = requestJSONviaGET("https://raw.githubusercontent.com/brunoshine/tropoSample/master/sample_data.json");
-// var data = [
-//     {
-//         "name":"Bruno Figueiredo", 
-//         "phone": "0351926368351",
-//         "lang":"en-GB", 
-//         "contacts": [{"name":"Bruno", "phone":"0351926368351"}]
-//     },
-//     {
-//         "name":"John Doe", 
-//         "lang":"pt-PT", 
-//         "contacts": [{"name":"Bruno", "phone":"0351926368351"}]
-//     }
-// ];
-
-var filteredData = data.filter(function(el){
-    el.phone == callerId;
-});
-
-// // if its a know caller id get his name, and conversation language
-// var knownCaller = null;
-// if(filteredData && filteredData.length === 1){
-//     knownCaller = filteredData[0];
-// }
-var knownCaller = data[0];
+var knownCaller = getCallerDetails(callerId);
 
 // greet user
-
-// translate to caller native language
-var welcomeMessage = "Hello Mr. " + knownCaller.name + ". Welcome to the Matrix.";
-say(welcomeMessage);
-log("Said welcome message to " + currentCall.callerID);
+greetUser(knownCaller);
 
 // ask to talk to account manager or operator
-var actionPickMessage = "With whom would you like to speak?";
-var actionPickOptions = "";
-if(knownCaller.contacts !== null){
-    for(var i=0; i < knownCaller.contacts.length; i++){
-        var contact = knownCaller.contacts[i];
-        var opt = i + 1;
-        // +=  IS NOT SUPPORTED
-        actionPickMessage = actionPickMessage + "Press " + opt + " for " + contact.name + ".";
-        actionPickOptions = actionPickOptions + opt + ","; 
-    }
-}
+var result = requestForAction(knownCaller);
 
-actionPickMessage = actionPickMessage + " To speak to the operator please choose 0.";
-actionPickOptions = actionPickOptions + "0";
-
-log("created options list");
-log(actionPickMessage);
-log(actionPickOptions);
-
-log("asking...");
-var result=ask(actionPickMessage, {
-   choices:actionPickOptions
-});
-log("picked operation:" + result.value);
-
-var operatorId = parseInt(result.value);
-var selectedOperator = "+351999000888"; // by default go to Operator.    
-
-if(operatorId !== 0){
-    selectedOperator = knownCaller.contacts[operatorId-1].phone;
-    say("You picked to speak to " + knownCaller.contacts[operatorId-1].name + ".");
-    log("said transfing to picked contact");
-}else{
-    say("You picked to speak to the Operator.");
-    log("said transferring to operator");
-}
-
-// redirect call to selected contact (AM or Operator)
-
-//translate transfer message to caller native language
-var transferMessage ="Transferring you now, please wait."; 
-
-// transfer
-say(transferMessage);
-//transfer(selectedOperator);
+redirectCall(knownCaller, result);
